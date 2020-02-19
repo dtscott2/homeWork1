64ab89f3b8eb3547b4e4c53196b02e189208dbe8
dustin <dtscott2@gmail.com>
2020-02-18 23:25:56 -0600

    Initial commit

---
+++ src/sample/Employee.java
@@ -0,0 +1,41 @@
+package sample;
+
+import java.util.UUID;
+
+public class Employee implements Worker
+{
+
+    public String firstName;
+    public String lastName;
+    public UUID id;
+    public boolean isActive;
+
+    @Override
+    public String toString()
+    {
+        return this.firstName + " " + this.lastName;
+    }
+
+
+    @Override
+    public void hire()
+    {
+        isActive = true;
+    }
+
+    @Override
+    public void fire()
+    {
+        isActive = false;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
